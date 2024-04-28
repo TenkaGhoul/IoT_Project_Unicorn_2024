21a6563d2b2481daaedabad46b84a751cf7e533a
CalValmar <vialat.paul@gmail.com>
2024-04-29 00:08:02 +0200

    set npm + dashboard frontend and backend api status

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,22 @@ const port = 3001;
 // Middleware to parse the body of the request
 app.use(bodyParser.json());
 
+// Routes (endpoints)
+app.get('/', (req, res) => {
+    res.send(`
+    <h1>Welcome to the SmartShade API</h1>
+    <h2>Please use <a href="http://localhost:3001/status">/status</a> to verify the connection to the API</h2>
+    `);
+});
+  
+// Status route
+app.get('/status', (req, res) => {
+    res.send(`
+    <h1>Server is running</h1>
+    <h2>API is ready to use</h2>
+    `);
+});
+
 // Routes
 app.use('/data', dataRoutes);
 app.use('/configuration', configRoutes);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './componements/dashboard/dashboard';
+
 import './App.css';
 
-function App() {
+function Main() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} /> 
+        <Route path="*" element="404 Not Found" />
+      </Routes>
     </div>
   );
 }
 
+function App() {
+  return (
+    <Router>
+      <Main />
+    </Router>
+  );
+}
 export default App;
